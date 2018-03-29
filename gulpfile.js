f2a33a192d0ff87a898f30c4921d2eda787fe58f
nakree1 <vexter3d@gmail.com>
2018-03-29 23:32:57 +0300

    Layout created

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+		watch = require('gulp-watch'),
+		plumber = require('gulp-plumber'),
+		autoprefix = require('gulp-autoprefixer'),
+		sass = require('gulp-sass'),
+		browserSync = require('browser-sync'),
+		csso = require('gulp-csso'),
+		uncss = require('gulp-uncss'),
+		rename = require('gulp-rename');
+
+gulp.task('sass', function() {
+	return gulp.src('scss/style.scss')
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		// .pipe(csso())
+		// .pipe(uncss({
+		// 	html: 'index.html'
+		// }))
+		.pipe(autoprefix(['last 10 versions']))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('build',function() {
+	return gulp.src('scss/bootstrap/bootstrap.scss')
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		// .pipe(csso())
+		// .pipe(uncss({
+		// 	html: 'index.html'
+		// }))
+		.pipe(autoprefix(['last 10 versions']))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            notify: false
+        }
+    });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('scss/*.scss', ['sass']);
+	gulp.watch(['index.html','js/main.js'], browserSync.reload);
+});
